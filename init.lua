f66ecd2be43cad82d9a90fc0461f5bf8f58d4df6
Fred Gobry <frederic.gobry@proton.me>
2024-08-09 13:13:55 -0400

    autoformat and support crate completion

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.cmd [[colorscheme koehler]]
+vim.cmd [[let g:rustfmt_autosave = 1]]
 
 require("config.lazy")
 
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -9,4 +9,25 @@ return {
 	    }
 	  end
 	},
+        {
+            'saecki/crates.nvim',
+            tag = 'stable',
+            config = function()
+                require('crates').setup{
+		  lsp = {
+                    enabled = true,
+                    actions = true,
+                    completion = true,
+                    hover = true,
+                  },
+		  completion = {
+		    crates = {
+                      enabled = true,
+                      max_results = 8,
+                      min_chars = 3,
+                    },
+		  },
+		}
+            end,
+        },
 }
