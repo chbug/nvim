b963c77efbbd65924088ea1670a548b354b8689f
Frederic Gobry <frederic.gobry@gmail.com>
2024-08-19 16:10:40 -0400

    add ripgrep plugin to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
         config = function()
           require('telescope').setup({})
 
